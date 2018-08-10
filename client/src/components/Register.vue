2cb3b192507a3c67ebc822e7991546168ca6b7e9
Zyle <poonyapat.y@ku.th>
2018-08-10 17:35:26 +0700

    Setting registration page

---
+++ client/src/components/Register.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <h1>Register</h1>
+        <input v-model="email" type="email" name="email" placeholder="email..."/>
+        <input v-model="password" type="password" name="password" placeholder="password..."/>
+        <button @click="register">Register</button>
+    </div>
+</template>
+
+<script>
+import AuthenticationService from "@/services/authenticationService"
+export default {
+  name: "Register",
+  data: () => {
+    return {
+      email: "",
+      password: ""
+    };
+  },
+  watch: {
+    email(newValue) {
+      console.log("email has changed to", newValue);
+    }
+  },
+  methods: {
+    async register() {
+      const response = await AuthenticationService.register({
+        email: this.email,
+        password: this.password
+      })
+      this.email = ""
+      this.password = ""
+      console.log(response.data.message)
+    }
+  },
+  mounted() {
+    setTimeout(()=>{
+      this.email = "hello world"
+    }, 3000)
+  },
+};
+</script>
--- server/src/app.js
+++ server/src/app.js
@@ -6,13 +6,13 @@ const cors = require('cors')
 const morgan = require('morgan')
 
 const app = express()
-app.use(morgan('combine'))
+app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
 
-app.get('/status', (req, res) => {
+app.post('/register', (req, res) => {
     res.send({
-        message: 'hello world'
+        message: `Hello ${req.body.email}! your user was registered! Have fun!`
     })
 })
 
