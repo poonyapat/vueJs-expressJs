c5cbac2dd921627276a4ffe0d010a19f8e66c2d8
Zyle <poonyapat.y@ku.th>
2018-08-10 20:17:33 +0700

    Fix Bug

---
+++ server/src/config/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    port: process.env.PORT || '8081',
+    db: {
+        database: process.env.DB_NAME || 'sa_project', 
+        user: process.env.DB_USER || 'root',
+        password: process.env.DB_PASSWORD || '',
+        options: {
+            dialect: process.env.DIALECT || 'mysql',
+            host: process.env.HOST || '127.0.0.1',
+            port: process.env.PORT || '3306'
+        }
+    }
+}
---
+++ server/src/models/user.js
@@ -0,0 +1,7 @@
+module.exports = (sequelize, DataTypes) => sequelize.define('User', {
+    username: {
+        type: DataTypes.STRING,
+        unique: true
+    },
+    password: DataTypes.STRING
+})
